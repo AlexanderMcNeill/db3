d7f2f74590fec20a67217ba019550e5a1f5bdeea
Alexander McNeill <mcnear1@student.op.ac.nz>
2015-10-17 17:36:38 +1300

    Adding commenting

--- Assignment_2/mcneill_db3_assignment2.py
+++ Assignment_2/mcneill_db3_assignment2.py
@@ -20,25 +20,28 @@ from sklearn.svm import SVC
 print("========================Exercise 1 - K Means========================")
 
 # Creating artificial data
-X_blobs, y_blobs = make_blobs(n_samples=300, centers=3,
+X, y = make_blobs(n_samples=300, centers=3,
                   random_state=0, cluster_std=0.60)
 
 # Displaying what the artificial data looks like
 plt.title("Artificial data")
-plt.scatter(X_blobs[:, 0], X_blobs[:, 1], s=50);
+plt.scatter(X[:, 0], X[:, 1], s=50);
 plt.show()
 
 # Creating k means with 3 clusters
 kmeans = KMeans(n_clusters=3, random_state=8)
 # Creating k means that fits the data and getting the labels for each point
-y_hat = kmeans.fit(X_blobs).labels_
+y_hat = kmeans.fit(X).labels_
 
+# Displaying the clustering that the kmeans has defined
 plt.title("Clustering after using k-Means")
-plt.scatter(X_blobs[:, 0], X_blobs[:, 1], c=y_hat)
+plt.scatter(X[:, 0], X[:, 1], c=y_hat)
 plt.show()
 
 print("========================Exercise 2 - K-Nearest neighbors========================")
+# reading in the data from the file
 vs = np.genfromtxt("video_store_2.csv", delimiter=",", names=True, dtype=(int, "|S1", float, int, int, float, "|S10", "|S3"))
+# Getting the data i want to use
 vs_records = vs[['Gender','Income','Age','Rentals','Avg_Per_Visit','Genre']]
 vs_names = vs_records.dtype.names
 vs_dict = [dict(zip(vs_names, record)) for record in vs_records]
@@ -46,46 +49,62 @@ vs_vec = DictVectorizer()
 X = vs_vec.fit_transform(vs_dict).toarray()
 y = vs['Incidentals']
 
+# Defining how many rows i want to be in my training set
 tpercent = 0.8
 tsize = tpercent * len(X)
+
+# Splitting the data into training and testing sets
 X_train = X[:tsize,:]
 X_test = X[tsize:,:]
 
 y_train = y[:tsize]
 y_test = y[tsize:]
 
+# Normalising all the data
 min_max_scaler = preprocessing.MinMaxScaler()
 X_train_norm = min_max_scaler.fit_transform(X_train)
 X_test_norm = min_max_scaler.fit_transform(X_test)
 
+# Creating the n K-Nearest neightbor model
 knn = KNeighborsClassifier(n_neighbors=5) 
+# Fitting the model to the video store training data
 knn.fit(X_train_norm, y_train)
-
-print knn.score(X_test_norm, y_test)
 
 print("========================Exercise 3 - Error analysis: Precision and recall========================\n")
 
+# Predicting the results with the K-Nearest neighbor model
 y_predict = knn.predict(X_test_norm)
+
+# Displaying report on how well it has performed
 print("Classification report for predictions on video store data set using K-Means")
 print metrics.classification_report(y_test, y_predict)
 
 print("========================Exercise 4 - Support vector machines========================\n")
 
+# Creating a support vector machine model
 clf = SVC(kernel='rbf')
+# Fitting the model to the video store training data
 clf.fit(X_train_norm, y_train)
+# Predicting the results of the test data using the model
 y_predict = clf.predict(X_test_norm)
+
+# Displaying a report on how well the support vector machines model worked
 print("Classification report for predictions on video store data set using logistic regession")
 print metrics.classification_report(y_test, y_predict)
 
 print("========================Exercise 5 - Logistic regression========================\n")
+# Creating a logistic regression model
 logreg = LogisticRegression()
+# Fitting the model to my data
 logreg.fit(X_train_norm, y_train)
+# Predicting the results of all of the test data
 y_predict = logreg.predict(X_test_norm)
 
+# Displaying a report on how well the logistic regression model performed
 print("Classification report for predictions on video store data set using logistic regession")
 print metrics.classification_report(y_test, y_predict)
 
-"""========================Exercise 6 - Principal Component Analysis========================"""
+print("========================Exercise 6 - Principal Component Analysis========================")
 
 
 print("========================Exercise 7 - Artificial neural networks========================"
@@ -93,6 +112,6 @@ print("========================Exercise 7 - Artificial neural networks==========
 + "\nthe result would be 10 what would map to be very close to one."
 + "\n\n1 * -10 + 1 * 20 + 0 * 20 = 10 ≈ 1")
 
-"""========================Exercise 8 - Validation========================"""
+print("========================Exercise 8 - Validation========================")
 
-"""========================Exercise 9 - Regularization========================"""
+print("========================Exercise 9 - Regularization========================")
