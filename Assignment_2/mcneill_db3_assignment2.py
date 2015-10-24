f666afbe305bcaee99c86d2e8f060fb3de5fd032
Alexander McNeill <mcnear1@student.op.ac.nz>
2015-10-24 14:34:06 +1300

    Fixing small print out issue in task 9

--- Assignment_2/mcneill_db3_assignment2.py
+++ Assignment_2/mcneill_db3_assignment2.py
@@ -180,7 +180,7 @@ print ridgeRegr.coef_
 """
 
 print("This code is showing the difference between using regularization and not using regularization."
-+"\nThe second model imposes a penalty upon each of the coefficients bringing them closer to zero,""
++"\nThe second model imposes a penalty upon each of the coefficients bringing them closer to zero,"
 +"\nthis is why the values are different."
 +"\nSquared weights penalizes large values more this is why the values in the second model are much smaller."
 )
